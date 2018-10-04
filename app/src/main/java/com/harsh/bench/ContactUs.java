a3c6e86bb8e1d9a2d51b0d1b1b88f693d8d1c8d2
Harsh <17it012@charusat.edu.in>
2018-10-05 02:32:11 +0530

    UI Improvements

--- app/src/main/java/com/harsh/bench/ContactUs.java
+++ app/src/main/java/com/harsh/bench/ContactUs.java
@@ -1,7 +1,10 @@
 package com.harsh.bench;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ContactUs extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+    }
+    public void onClick(View v){
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto","email@email.com", null));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "hi");
+        intent.putExtra(Intent.EXTRA_TEXT, "hi");
+        startActivity(Intent.createChooser(intent, "Choose an Email client :"));
     }
 }
